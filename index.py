e8ef8cf5eaa3105c7d7069719bae0d66fb3452e4
technoranch <108231159+technoranch@users.noreply.github.com>
2023-03-06 00:36:10 +0530

    Update index.py

--- index.py
+++ index.py
@@ -9,7 +9,7 @@ from movies_scraper import search_movies, get_movie
 
 
 TOKEN = os.getenv("TOKEN")
-URL = "https://movies-bot-inky.vercel.app"
+URL = "https://tg-ftl-bot.vercel.app"
 bot = Bot(TOKEN)
 
 
